dd474566e1c9ce0fb456d1c4242aa58591525868
wesley <wesleyvc2110@gmail.com>
2022-05-27 15:39:44 -0300

    alteracao no cadastrar.js

--- JavaScript/cadastrar.js
+++ JavaScript/cadastrar.js
@@ -1,11 +1,39 @@
-const nome = document.querySelector(input)
-const btnCadastrar = document.querySelector("input.btnPrincipal")
+const senha1 = document.querySelector("#senha1")
+const senha2 = document.querySelector("#senha2")
+const nome = document.querySelector("#nome")
+const email = document.querySelector("#email")
+const btnCadastrar = document.querySelector(".btnPrincipal")
 
-btnCadastrar.addEventListener("click", msgValidacao())
 
-function msgValidacao() { 
-if (nome.value == "" || email.value == "") {
-    alert("ERRO! Preencha todos os campos obrigatórios!")
-}else{
-alert("Deu certo!")
-}}
+
+btnCadastrar.addEventListener("click", confirmarSenha1IgualA2)
+
+ function msgValidacao() {
+    if( senhas(senha1.value, senha2.value) ){
+
+        if (nome.value == "" || email.value == "" || senha2.value == "")
+         alert("Preencha todos os campos obrigatórios!")
+        }else{
+         alert("Tudo certo!", nome.value, email.value)
+    }
+    
+}
+
+// function confirmarSenha1IgualA2(callback) {
+//     if(senha1.value != senha2.value){
+// alert('As senhas não conferem/nDigite a segunda senha igual a primeira senha')
+// senha2.value = ""
+// return false
+// }else{
+//     return true
+//     callback()
+// }
+// }
+
+function senhas(senha1, senha2){
+    if(senha1 == senha2){
+        return true
+    }else{
+        return false
+    }
+}
