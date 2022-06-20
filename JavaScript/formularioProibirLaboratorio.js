a1910eacf9db851b0b29c53723819db1c7969f24
Allan <allanb3779@gmail.com>
2022-06-19 21:04:18 -0300

    criando página de formulário para criar turma

--- JavaScript/formularioProibirLaboratorio.js
+++ JavaScript/formularioProibirLaboratorio.js
@@ -6,5 +6,6 @@ cancelar.addEventListener('click', function(){
 })
 function submitForm(event){
     event.preventDefault(); 
+    alert('dados enviados com sucesso')
     window.location.assign("http://127.0.0.1:5500/autenticacao/cadastro.html")
 }
