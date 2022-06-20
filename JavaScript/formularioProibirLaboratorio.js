b0ddcb702a612798e8c8779caa05a9a5420b9c7d
Allan <allanb3779@gmail.com>
2022-06-20 00:22:18 -0300

    Alterações nos formulário

--- JavaScript/formularioProibirLaboratorio.js
+++ JavaScript/formularioProibirLaboratorio.js
@@ -5,7 +5,21 @@ cancelar.addEventListener('click', function(){
     window.location.assign("http://127.0.0.1:5500/autenticacao/cadastro.html");
 })
 function submitForm(event){
-    event.preventDefault(); 
+    event.preventDefault();
+    var url_atual = window.location.href; 
     alert('dados enviados com sucesso')
-    window.location.assign("http://127.0.0.1:5500/autenticacao/cadastro.html")
+    window.location.assign("http://127.0.0.1:5500/")
+    
+    if(url_atual == "https://ifpi-picos.github.io/acompi-front-end/usuarios/aluno/formularioReservaComputador.html"){
+    window.location.assign("https://ifpi-picos.github.io/acompi-front-end/usuarios/aluno/escolherTurma.html")
+    }
+    if(url_atual == "https://ifpi-picos.github.io/acompi-front-end/usuarios/professor/formularioCriarTurma.html"){
+        window.location.assign("https://ifpi-picos.github.io/acompi-front-end/usuarios/professor/turmas.html")
+    }
+    if(url_atual == ""){
+        window.location.assign("http://127.0.0.1:5500/")
+    }
+    if(url_atual == ""){
+        window.location.assign("http://127.0.0.1:5500/")
+    }
 }
