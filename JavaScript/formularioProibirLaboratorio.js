c4d37f10e8ea6f6642acc6dc978ab421bd38112b
Allan Barbosa <allanb3779@gmail.com>
2022-11-02 22:52:54 -0300

    mostrando os dados

--- JavaScript/formularioProibirLaboratorio.js
+++ JavaScript/formularioProibirLaboratorio.js
@@ -60,6 +60,7 @@ function submitForm(event){
                     alert('dados enviados com sucesso')
                     window.location.href="ver-cancelar-reservas.html"
                 }else{
+                    console.log(dados)
                     console.log('Erro ao reservar computador')
                 }
             }catch(erro){
