3e11a2295978dd6cbdd4e25a26105b5926b78dbc
IagoBrenoAD <wesleyveloso30>
2022-12-01 11:32:16 -0300

    Ajeitando alert

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,13 +8,19 @@ const botaoCadastro = document.querySelector('#btnCadastro')
 
 //quando clicar no botao cadastrar
 //verificar se todos os campos do cadastro estao preenchidos
-function validandoCamposCadastro(event) {
+function validandoCamposCadastro (event) {
   if (nomeCadastro.value != '') {
     if (nomeCadastro.value.length >= 3) {
       if (emailCadastro.value != '') {
-        if (emailCadastro.value.indexOf('@ifpi.edu.br') != -1 || emailCadastro.value.indexOf('@aluno.ifpi.edu.br') != -1) {
+        if (
+          emailCadastro.value.indexOf('@ifpi.edu.br') != -1 ||
+          emailCadastro.value.indexOf('@aluno.ifpi.edu.br') != -1
+        ) {
           if (senha1Cadastro.value != '') {
-            if (senha1Cadastro.value.length >= 8 && senha1Cadastro.value.length <= 12) {
+            if (
+              senha1Cadastro.value.length >= 8 &&
+              senha1Cadastro.value.length <= 12
+            ) {
               if (senha2Cadastro.value != '') {
                 enviarMsgCadastro(
                   nomeCadastro,
@@ -47,7 +53,7 @@ function validandoCamposCadastro(event) {
 }
 
 //emitir alerta avisando se estar tudo certo ou se a senha1 esta diferente da senha2
-function enviarMsgCadastro(nome, email, senha1, senha2, event) {
+function enviarMsgCadastro (nome, email, senha1, senha2, event) {
   if (validaSenha(senha1Cadastro, senha2Cadastro)) {
     // mudar a pagina
     submitForm(event)
@@ -59,114 +65,124 @@ function enviarMsgCadastro(nome, email, senha1, senha2, event) {
 }
 
 //nao envia o formulario e navega entre as paginas mudando a url
-async function submitForm(event) {
-  event.preventDefault();
-  var url_atual = window.location.pathname;
-  if (url_atual.endsWith("autenticacao/cadastro.html")) {
-      const dados = getDadosForm()
-      enviarDados(dados)
-      function getDadosForm() {
-        const nomeCadastro = document.querySelector('#nomeCadastro')
-        const emailCadastro = document.querySelector('#emailCadastro')
-        const senha1Cadastro = document.querySelector('#senha1Cadastro')
-        if (nomeCadastro.value === null || emailCadastro.value === null || senha1Cadastro.value === null) {
-          console.log('campos vazios')
-          return
-        }
-        const dados = {
-          nome: nomeCadastro.value,
-          senha: senha1Cadastro.value,
-          email: emailCadastro.value,
-        }
-        return dados
+async function submitForm (event) {
+  event.preventDefault()
+  var url_atual = window.location.pathname
+  if (url_atual.endsWith('autenticacao/cadastro.html')) {
+    const dados = getDadosForm()
+    enviarDados(dados)
+    function getDadosForm () {
+      const nomeCadastro = document.querySelector('#nomeCadastro')
+      const emailCadastro = document.querySelector('#emailCadastro')
+      const senha1Cadastro = document.querySelector('#senha1Cadastro')
+      if (
+        nomeCadastro.value === null ||
+        emailCadastro.value === null ||
+        senha1Cadastro.value === null
+      ) {
+        console.log('campos vazios')
+        return
       }
-      async function enviarDados(dados) {
-        try {
-          const resposta = await fetch('https://acompi-back-end-la29.onrender.com/cadastro', {
+      const dados = {
+        nome: nomeCadastro.value,
+        senha: senha1Cadastro.value,
+        email: emailCadastro.value
+      }
+      return dados
+    }
+    async function enviarDados (dados) {
+      try {
+        const resposta = await fetch(
+          'https://acompi-back-end-la29.onrender.com/cadastro',
+          {
             method: 'POST',
             headers: {
               Accept: 'application/json',
-              'Content-Type': 'application/json',
+              'Content-Type': 'application/json'
             },
             body: JSON.stringify(dados)
-          })
-          if (resposta.status === 201) {
-            document.querySelector('#senha1Cadastro').value = ''
-            document.querySelector('#senha2Cadastro').value = ''
-            document.querySelector('#nomeCadastro').value = ''
-            document.querySelector('#emailCadastro').value = ''
-            alert('usuário cadatrado, clique no link enviado ao email para validar a conta')
-            window.location.href = "login.html"
-          } else {
-            alert('este email já possui uma conta cadastrada')
-            console.log('Erro ao realizar cadastro')
           }
-        } catch (erro) {
-          console.error(erro)
+        )
+        if (resposta.status === 201) {
+          document.querySelector('#senha1Cadastro').value = ''
+          document.querySelector('#senha2Cadastro').value = ''
+          document.querySelector('#nomeCadastro').value = ''
+          document.querySelector('#emailCadastro').value = ''
+          alert(
+            'usuário cadatrado, clique no link enviado ao email para validar a conta'
+          )
+          window.location.href = 'login.html'
+        } else {
+          alert('este email já possui uma conta cadastrada')
+          console.log('Erro ao realizar cadastro')
         }
+      } catch (erro) {
+        console.error(erro)
       }
-  };
+    }
+  }
 
-  if (window.location.href.endsWith("autenticacao/validacao.html")) {
-    window.location.href = "login.html"
-  } else if (window.location.href.endsWith("autenticacao/login.html")) {
+  if (window.location.href.endsWith('autenticacao/validacao.html')) {
+    window.location.href = 'login.html'
+  } else if (window.location.href.endsWith('autenticacao/login.html')) {
     try {
       const usuario = {
-          email: emailLogin.value,
-          senha: senhaLogin.value
+        email: emailLogin.value,
+        senha: senhaLogin.value
       }
-      const resp = await fetch('https://acompi-back-end-la29.onrender.com/login', {
+      const resp = await fetch(
+        'https://acompi-back-end-la29.onrender.com/login',
+        {
           method: 'POST',
           headers: {
-              Accept: 'application/json',
-              'Content-type': 'application/json'
+            Accept: 'application/json',
+            'Content-type': 'application/json'
           },
           body: JSON.stringify(usuario)
-      })
-      resp.json();
+        }
+      )
+      resp.json()
       if (resp.status === 201) {
         alert('Sucesso')
-        window.location.href = "../usuarios/aluno/professores.html"
+        window.location.href = '../usuarios/aluno/professores.html'
       } else {
-          alert(resp.message)
+        alert(resp.message)
       }
-  } catch (error) {
-     alert(error.message)
-}
-  } else if (window.location.href.endsWith("autenticacao/modificar-senha.html")) {
+    } catch (error) {
+      alert(error.message)
+    }
+  } else if (
+    window.location.href.endsWith('autenticacao/modificar-senha.html')
+  ) {
     try {
       const usuario = {
-          email: emailModificarSenha.value,
-          senha: senha1ModificarSenha.value,
-          confirmasenha: senha2ModificarSenha.value,
+        email: emailModificarSenha.value,
+        senha: senha1ModificarSenha.value,
+        confirmasenha: senha2ModificarSenha.value
       }
       const resp = await fetch('http://localhost:3000/modificar-senha', {
-          method: 'PATCH',
-          headers: {
-              Accept: 'application/json',
-              'Content-type': 'application/json'
-          },
-          body: JSON.stringify(usuario)
+        method: 'PATCH',
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
       })
       resp.json()
       if (resp.status === 201) {
-        alert('TudoCertto')
-        location.href = "login.html"
-      
+        alert('Tudo Certo')
+        location.href = 'login.html'
       } else {
-          console.log(resp)
-      
+        console.log(resp)
       }
-  } catch (error) {
+    } catch (error) {
       alert(resp.message)
-}}
-
+    }
   }
-
-
+}
 
 //verificar se a senha1 digitada esta igual a senha2
-function validaSenha(senha1, senha2) {
+function validaSenha (senha1, senha2) {
   if (senha1.value == senha2.value) {
     return true
   } else {
@@ -178,10 +194,10 @@ function validaSenha(senha1, senha2) {
 
 //tela de verificação de codigo
 
-const codigoVerificacao = document.getElementById("codigoVerificacao")
+const codigoVerificacao = document.getElementById('codigoVerificacao')
 
-function enviarMsgValidacao(event) {
-  if (codigoVerificacao.value != "") {
+function enviarMsgValidacao (event) {
+  if (codigoVerificacao.value != '') {
     alert(
       'Tudo certo!\nCodigo de Validação correto!  ' + codigoVerificacao.value
     )
@@ -189,10 +205,10 @@ function enviarMsgValidacao(event) {
     submitForm(event)
   } else {
     alert('Digite o código no campo solicitado!')
-    codigoVerificacao.style.boxShadow = "3px 3px 28px #ee6a6e";
+    codigoVerificacao.style.boxShadow = '3px 3px 28px #ee6a6e'
     window.setTimeout(function () {
-      codigoVerificacao.style.boxShadow = 'none';
-    }, 4000);
+      codigoVerificacao.style.boxShadow = 'none'
+    }, 4000)
   }
 }
 
@@ -200,14 +216,16 @@ function enviarMsgValidacao(event) {
 
 // tela ver e cancelar reserva (aluno)
 // funçao para o butão mudar
-function cancelarReserva(numeroParaIdentificarTurma) {
-  const btnCancelarReserva = document.getElementsByClassName("btnCancelarReserva")[numeroParaIdentificarTurma]
-  if (btnCancelarReserva.innerHTML == "Cancelar Reserva") {
-    btnCancelarReserva.style.backgroundColor = "#3da9fc"
-    btnCancelarReserva.innerHTML = "Reservar Turma"
+function cancelarReserva (numeroParaIdentificarTurma) {
+  const btnCancelarReserva = document.getElementsByClassName(
+    'btnCancelarReserva'
+  )[numeroParaIdentificarTurma]
+  if (btnCancelarReserva.innerHTML == 'Cancelar Reserva') {
+    btnCancelarReserva.style.backgroundColor = '#3da9fc'
+    btnCancelarReserva.innerHTML = 'Reservar Turma'
   } else {
-    btnCancelarReserva.style.backgroundColor = "#c54141"
-    btnCancelarReserva.innerHTML = "Cancelar Reserva";
+    btnCancelarReserva.style.backgroundColor = '#c54141'
+    btnCancelarReserva.innerHTML = 'Cancelar Reserva'
   }
 }
 // fim tela ver e cancelar reserva (aluno)
@@ -215,24 +233,26 @@ function cancelarReserva(numeroParaIdentificarTurma) {
 // tela de ver e controlar alunos (professor)
 
 // função para dar cor a msg 'possui computador'
-const possuiComp = document.querySelectorAll("h4.ausenciaComputador")
+const possuiComp = document.querySelectorAll('h4.ausenciaComputador')
 possuiComp.forEach(function (possuirCom) {
-  if (possuirCom.innerHTML == "Possui Computador") {
-    possuirCom.style.color = "#3da9fc";
+  if (possuirCom.innerHTML == 'Possui Computador') {
+    possuirCom.style.color = '#3da9fc'
   } else {
-    possuirCom.style.color = "red";
+    possuirCom.style.color = 'red'
   }
 })
 
 // funçao para o butão mudar
-function removerAluno(numeroParaIdentificarAluno) {
-  const btnRemoverAluno = document.querySelectorAll("button.btnRemoverAluno")[numeroParaIdentificarAluno]
-  if (btnRemoverAluno.innerHTML == "Remover Aluno") {
-    btnRemoverAluno.style.backgroundColor = "#3da9fc"
-    btnRemoverAluno.innerHTML = "Readicionar Aluno"
+function removerAluno (numeroParaIdentificarAluno) {
+  const btnRemoverAluno = document.querySelectorAll('button.btnRemoverAluno')[
+    numeroParaIdentificarAluno
+  ]
+  if (btnRemoverAluno.innerHTML == 'Remover Aluno') {
+    btnRemoverAluno.style.backgroundColor = '#3da9fc'
+    btnRemoverAluno.innerHTML = 'Readicionar Aluno'
   } else {
-    btnRemoverAluno.style.backgroundColor = "#c54141"
-    btnRemoverAluno.innerHTML = "Remover Aluno";
+    btnRemoverAluno.style.backgroundColor = '#c54141'
+    btnRemoverAluno.innerHTML = 'Remover Aluno'
   }
 }
 
@@ -244,10 +264,10 @@ function removerAluno(numeroParaIdentificarAluno) {
 const emailLogin = document.querySelector('#emailLogin')
 const senhaLogin = document.querySelector('#senhaLogin')
 
-async function enviarMensagemLogin(event) {
+async function enviarMensagemLogin (event) {
   if (emailLogin.value != '') {
     if (senhaLogin.value != '') {
-        submitForm(event)
+      submitForm(event)
     } else {
       alert('Preencha a senha!')
     }
@@ -258,66 +278,64 @@ async function enviarMensagemLogin(event) {
 
 //---------------fim tela de Login---------------//
 
-
 //---------------Início da tela de Modificar senha---------------//
 const emailModificarSenha = document.querySelector('#emailModificarSenha')
 const senha1ModificarSenha = document.querySelector('#senha1ModificarSenha')
 const senha2ModificarSenha = document.querySelector('#senha2ModificarSenha')
 
-async function enviarMensagemModificarSenha(event) {
+async function enviarMensagemModificarSenha (event) {
   if (emailModificarSenha.value != '') {
     if (senha1ModificarSenha.value != '') {
       if (senha2ModificarSenha.value != '') {
         if (validaSenha(senha1ModificarSenha, senha2ModificarSenha)) {
           alert('Tudo certo!')
           submitForm(event)
-          
         } else {
           alert('A senha de confirmação precisa ser igual a primeira senha!')
           senha1ModificarSenha.value = ''
           senha2ModificarSenha.value = ''
         }
       } else {
-        alert("Preencha a senha de confirmação")
+        alert('Preencha a senha de confirmação')
       }
     } else {
-      alert("Preencha a senha")
+      alert('Preencha a senha')
     }
   } else {
-    alert("Preencha o E-mail!")
+    alert('Preencha o E-mail!')
   }
-
 }
 
-async function enviarParaApi(){
-  alert("foi")
+async function enviarParaApi () {
+  alert('foi')
   try {
-      const usuario = {
-          email: emailModificarSenha.value,
-          senha: senha1ModificarSenha.value,
-          confirmasenha: senha2ModificarSenha.value,
-      }
-      alert('fjbhdfjbhdflkgkdjghfghdfjghdjhgfghguhepgijengoerhgerhogierg')
-      const resp = await fetch('http://localhost:3000/modificar-senha', {
-          method: 'PATCH',
-          headers: {
-              Accept: 'application/json',
-              'Content-type': 'application/json'
-          },
-          body: JSON.stringify(usuario)
-      })
-      resp.json()
-      console.log(resp.status)
-      if (resp.status === 201) {
-        alert(resp)
-        return true
-      } else {
-          alert(resp)
-          return false
-      }
-  } catch (error) {
-      console.error(error.message)
+    const usuario = {
+      email: emailModificarSenha.value,
+      senha: senha1ModificarSenha.value,
+      confirmasenha: senha2ModificarSenha.value
+    }
+    alert('fjbhdfjbhdflkgkdjghfghdfjghdjhgfghguhepgijengoerhgerhogierg')
+    const resp = await fetch('http://localhost:3000/modificar-senha', {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(usuario)
+    })
+    resp.json()
+    console.log(resp.status)
+    if (resp.status === 201) {
+      alert(resp)
+      return true
+    } else {
+      alert(resp)
       return false
-}}
+    }
+  } catch (error) {
+    console.error(error.message)
+    return false
+  }
+}
 
 //----------------------------------------------------------------------------------------------------------- //
