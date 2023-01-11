02ecaecdd22751ea85d360593644fc6c89292bd8
Allan Barbosa <allanb3779@gmail.com>
2023-01-11 14:41:05 -0300

    alterando a página do aluno ver turma para só mostrar turmas com vaga de reserva

--- JavaScript/aluno-turmas.js
+++ JavaScript/aluno-turmas.js
@@ -6,11 +6,13 @@ async function getTurmas() {
 }
 function preencherTurmas(turmas) {
     turmas.forEach(async turma => {
-        const res = await fetch('https://acompi-back-end-la29.onrender.com/turmas/professor/' + turma.id_professor)
+        const res = await fetch('https://acompi-back-end-la29.onrender.com/turmas/professor/' + turma.id_professor.toString())
         const professor = await res.json()
         const alunoID = 35;
         const verificaReserva = verificarReserva(turma.reservas, alunoID);
-        if (verificaReserva != true) {
+        const retorno = await fetch('https://acompi-back-end-la29.onrender.com/reservas/' + turma.id.toString())
+        const quantidadeReservaTurma = await retorno.json()
+        if (verificaReserva != true && quantidadeReservaTurma < 12) {
             const novaTurmaHTML = '<div>\n<h1>Turma ' + turma.id + '</h1>\n<p>Laboratório: ' + turma.id_lab + '</p>\n<p>Professor: ' + professor[0].nome + '</p>\n<p>Horário: ' + turma.horario_inicio + 'min às ' + turma.horario_fim + 'min</p>\n<p>Data: ' + turma.data_turma + '</p>\n<a class="index" href="formulario-reserva-computador.html?turma=' + turma.id + '">Entrar na turma</a>\n</div>'
             divTurmas.innerHTML = divTurmas.innerHTML + novaTurmaHTML
         }
