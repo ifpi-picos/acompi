b12577b19a8a181137aaa3831411cb3a12e78c57
Allan Barbosa <allanb3779@gmail.com>
2023-01-11 01:42:49 -0300

    alterando o javascript da página do aluno ver as turmas

--- JavaScript/aluno-turmas.js
+++ JavaScript/aluno-turmas.js
@@ -1,16 +1,27 @@
 const divTurmas = document.querySelector('#turmas')
-
-async function getTurmas () {
+async function getTurmas() {
     const res = await fetch('https://acompi-back-end-la29.onrender.com/turmas');
     const turmas = await res.json();
     preencherTurmas(turmas);
 }
-function preencherTurmas (turmas) {
-turmas.forEach(async turma => {
-    const res = await fetch('https://acompi-back-end-la29.onrender.com/turmas/professor/'+turma.id_professor)
-    const professor = await res.json()
-    const novaTurmaHTML = '<div>\n<h1>Turma '+turma.id+'</h1>\n<p>Laboratório: '+turma.id_lab+'</p>\n<p>Professor: '+professor[0].nome+'</p>\n<p>Horário: '+turma.horario_inicio+'min às '+turma.horario_fim+'min</p>\n<p>Data: '+turma.data_turma+'</p>\n<a class="index" href="formulario-reserva-computador.html?turma='+turma.id+'">Entrar na turma</a>\n</div>'
-    divTurmas.innerHTML = divTurmas.innerHTML + novaTurmaHTML
-});
+function preencherTurmas(turmas) {
+    turmas.forEach(async turma => {
+        const res = await fetch('https://acompi-back-end-la29.onrender.com/turmas/professor/' + turma.id_professor)
+        const professor = await res.json()
+        const alunoID = 35;
+        const verificaReserva = verificarReserva(turma.reservas, alunoID);
+        if (verificaReserva != true) {
+            const novaTurmaHTML = '<div>\n<h1>Turma ' + turma.id + '</h1>\n<p>Laboratório: ' + turma.id_lab + '</p>\n<p>Professor: ' + professor[0].nome + '</p>\n<p>Horário: ' + turma.horario_inicio + 'min às ' + turma.horario_fim + 'min</p>\n<p>Data: ' + turma.data_turma + '</p>\n<a class="index" href="formulario-reserva-computador.html?turma=' + turma.id + '">Entrar na turma</a>\n</div>'
+            divTurmas.innerHTML = divTurmas.innerHTML + novaTurmaHTML
+        }
+    });
+    if (divTurmas.innerHTML == '\n    ') {
+        alert('Sem turmas')
+    }
+}
+function verificarReserva(reservas, alunoID) {
+    reservas.forEach(reserva => {
+        if (resserva.id_aluno == alunoID) return true;
+    })
 }
 getTurmas()
