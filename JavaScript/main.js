7c8589993936e825415678c47cd5a306851b3524
Allan Barbosa <allanb3779@gmail.com>
2023-01-13 03:12:16 -0300

    mudando o caminho de redirecionamento da página inválida do login

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -15,14 +15,14 @@ if(localStorage.getItem("acesso")== "true"){
 }
 if (logado != true){
     alert("você não está autenticado");
-    window.location.href = '../autenticacao/login.html'
+    window.location.href = '../../autenticacao/login.html'
 }
 var url_atual = window.location.pathname
 if (token.user == "aluno" && url_atual.endsWith("usuarios/professor/formulario-criar-turma.html") || token.user == "aluno" && url_atual.endsWith("usuarios/professor/ver-cancelar-turma.html") || token.user == "aluno" && url_atual.endsWith("usuarios/professor/ver-remover-alunos-da-turma.html")){
-    window.location.href = '../autenticacao/login.html'
+    window.location.href = '../../autenticacao/login.html'
 }
 if (token.user == "professor" && url_atual.endsWith("usuarios/aluno/escolher-turma.html") || token.user == "professor" && url_atual.endsWith("usuarios/aluno/formulario-reserva-computador.html") || token.user == "professor" && url_atual.endsWith("usuarios/aluno/ver-cancelar-reservas.html")){
-    window.location.href = '../autenticacao/login.html'
+    window.location.href = '../../autenticacao/login.html'
 }
 function logout(){
     localStorage.setItem("token", "")
