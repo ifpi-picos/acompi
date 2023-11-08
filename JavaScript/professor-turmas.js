65c7410a994b54e2ca96d1acfdcc5f13721f8ffd
Allan Barbosa <allanb3779@gmail.com>
2023-11-08 20:14:32 -0300

    alterando a div turmas de professor

--- JavaScript/professor-turmas.js
+++ JavaScript/professor-turmas.js
@@ -12,7 +12,7 @@ function preencherTurmas(turmas) {
         const res = await fetch('https://acompi-back-end-lhbe.onrender.com/turmas/professor/' + turma.id_professor.toString());
         const professor = await res.json();
         const novaTurmaHTML = '<div>\n<h1>Laboratório ' + turma.id_lab + '</h1>\n<p>Professor: ' + professor[0].nome + '</p>\n<p>Horário: ' + turma.horario_inicio + 'min às ' + turma.horario_fim + 'min</p>\n<p>Data: ' + turma.data_turma + '</p>\n<a href="ver-remover-alunos-da-turma.html?turma=' + turma.id + '" class="index">Alunos</a>\n<a onclick="excluirTurma(' + turma.id + ')" class="index excluirTurma" id="excluirTurma">Excluir turma</a>\n</div>'
-        divTurmas.innerHTML = divTurmas.innerHTML + novaTurmaHTML
+        divTurmas.innerText = divTurmas.innerText + novaTurmaHTML
     });
 }
 getTurmas()
